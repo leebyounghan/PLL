5d37ae339e0f2eaa7d3deb578f5b552ceb020cd1
leebyounghan <qudgks96@ajou.ac.kr>
2022-05-31 15:31:28 +0900

    531

--- CLAD-master/src/run_scripts/single.sh
+++ CLAD-master/src/run_scripts/single.sh
@@ -1,70 +1,50 @@
-for cluster in $(seq 4 20)
-do
-    for seed in $(seq 0 4)
-    do
-      python ../main.py --data_path '../../data' --dataset_name 'snips' \
-          --cluster_type 'SCCL' --cluster_num $cluster --normal_class_index_list $seed\
-          --cluster_model_train_epochs 3 --classifier_epochs 0\
-          --cluster_model_train_lr 3e-6 --classifier_lr 3e-5\
-          --classifier_type 'DEC_bert' --temperature 1000 --perturbation 0.001\
-          --cluster_loss_on True --gpu_device 1 --GT False --pooling 'mean' --alpha 1.0\
-          --cluster_model_batch_size 32 --max_length 128 --iter 500  --language_model 'roberta-base'
-    done
+# for cluster in $(seq 2 20)
+# do
+    # for seed in $(seq 0 4)
+    # do
+      # python ../main.py --data_path '../../data' --dataset_name 'snips' \
+          # --cluster_type 'SCCL' --cluster_num $cluster --normal_class_index_list $seed\
+          # --cluster_model_train_epochs 3 --classifier_epochs 1\
+          # --cluster_model_train_lr 3e-6 --classifier_lr 3e-5\
+          # --classifier_type 'DEC_bert' --temperature 1000 --perturbation 0.001\
+          # --cluster_loss_on True --gpu_device 1 --GT False --pooling 'mean' --alpha 1.0\
+          # --cluster_model_batch_size 32 --max_length 128 --iter 500  --language_model 'roberta-base'
+    # done
+#
+    # for seed in $(seq 0 4)
+    # do
+      # python ../main.py --data_path '../../data' --dataset_name 'snips' \
+          # --cluster_type 'DEC_bert' --cluster_num $cluster --normal_class_index_list $seed\
+          # --cluster_model_train_epochs 3 --classifier_epochs 1\
+          # --cluster_model_train_lr 3e-6 --classifier_lr 3e-5\
+          # --classifier_type 'DEC_bert' --temperature 1000 --perturbation 0.001\
+          # --cluster_loss_on True --gpu_device 1 --GT False --pooling 'mean' --alpha 1.0\
+          # --cluster_model_batch_size 32 --max_length 128 --iter 500   --language_model 'roberta-base'
+      # done
+# done
 
-    for seed in $(seq 0 4)
-    do
-      python ../main.py --data_path '../../data' --dataset_name 'snips' \
-          --cluster_type 'DEC_bert' --cluster_num $cluster --normal_class_index_list $seed\
-          --cluster_model_train_epochs 3 --classifier_epochs 0\
-          --cluster_model_train_lr 3e-6 --classifier_lr 3e-5\
-          --classifier_type 'DEC_bert' --temperature 1000 --perturbation 0.001\
-          --cluster_loss_on True --gpu_device 1 --GT False --pooling 'mean' --alpha 1.0\
-          --cluster_model_batch_size 32 --max_length 128 --iter 500   --language_model 'roberta-base'
-      done
+#
+
+
+#
+for cluster in $(seq 10 14) #10까진 해놈
+do
+  python ../main.py --data_path '../../data' --dataset_name 'rostd' \
+      --cluster_type 'SCCL' --cluster_num $cluster --normal_class_index_list 0\
+      --cluster_model_train_epochs 1 --classifier_epochs 0\
+      --cluster_model_train_lr 3e-6 --classifier_lr 3e-5\
+      --classifier_type 'DEC_bert' --temperature 1000 --perturbation 0.001\
+      --cluster_loss_on True --gpu_device 1 --GT False --pooling 'mean' --alpha 1.0\
+      --cluster_model_batch_size 32 --max_length 128 --iter 500  --language_model 'roberta-base'
 done
 
-#
-#
-#
-#
-# for cluster in $(seq 1 1)
-# do
-  # python ../main.py --data_path '../../data' --dataset_name 'snips' \
-      # --cluster_type 'SCCL' --cluster_num 5 --normal_class_index_list 0\
-      # --cluster_model_train_epochs 1 --classifier_epochs 0\
-      # --cluster_model_train_lr 3e-6 --classifier_lr 3e-5\
-      # --classifier_type 'DEC_bert' --temperature 1000 --perturbation 0.001\
-      # --cluster_loss_on True --gpu_device 1 --GT False --pooling 'mean' --alpha 1.0\
-      # --cluster_model_batch_size 32 --max_length 128 --iter 500 # --language_model 'roberta-base'
-# done
-#
-#
-# for cluster in $(seq 1 1)
-# do
-  # python ../main.py --data_path '../../data' --dataset_name 'snips' \
-      # --cluster_type 'DEC_bert' --cluster_num 5 --normal_class_index_list 0\
-      # --cluster_model_train_epochs 1 --classifier_epochs 1\
-      # --cluster_model_train_lr 3e-6 --classifier_lr 3e-5\
-      # --classifier_type 'DEC_bert' --temperature 1000 --perturbation 0.001\
-      # --cluster_loss_on True --gpu_device 1 --GT False --pooling 'mean' --alpha 1.0\
-      # --cluster_model_batch_size 32 --max_length 128 --iter 500 # --language_model 'roberta-base'
-# done
-#
-#
-
-#
-#
-
-#
-# for cluster in $(seq 1 20)
-# do
-  # python ../main.py --data_path '../../data' --dataset_name 'sst' \
-      # --cluster_type 'SCCL' --cluster_num $cluster --normal_class_index_list  0 \
-      # --cluster_model_train_epochs 0 --classifier_epochs 0\
-      # --cluster_model_train_lr 3e-6 --classifier_lr 3e-5\
-      # --classifier_type 'DEC_bert' --temperature 1000 --perturbation 0.001\
-      # --cluster_loss_on True --gpu_device 1 --GT False --pooling 'mean' --alpha 1.0\
-      # --cluster_model_batch_size 4 --max_length 498 --iter 500  --language_model 'roberta-base'
-# done
-
-#
+for cluster in $(seq 10 14)
+do
+  python ../main.py --data_path '../../data' --dataset_name 'rostd' \
+      --cluster_type 'SCCL' --cluster_num $cluster --normal_class_index_list 0\
+      --cluster_model_train_epochs 1 --classifier_epochs 1\
+      --cluster_model_train_lr 3e-6 --classifier_lr 3e-5\
+      --classifier_type 'DEC_bert' --temperature 1000 --perturbation 0.001\
+      --cluster_loss_on True --gpu_device 1 --GT False --pooling 'mean' --alpha 1.0\
+      --cluster_model_batch_size 32 --max_length 128 --iter 500  --language_model 'roberta-base'
+done
