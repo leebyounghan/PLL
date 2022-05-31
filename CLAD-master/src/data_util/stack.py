7e5be715acb59fcea54ba559eb60f6a998f9d3f6
leebyounghan <qudgks96@ajou.ac.kr>
2022-05-31 17:07:32 +0900

    data load ++

---
+++ CLAD-master/src/data_util/stack.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import json
+import numpy as np
+import torch
+import pdb
+import os
+import pdb
+import config
+
+def bank_Dataset():
+    path = "/../../data/stackoverflow/seed_" + str(config.normal_class_index_list[0])
+    print(path)
+    cwd = os.getcwd()
+    data_train = pd.read_csv(cwd + path + "/train.csv")
+    test = pd.read_csv(cwd + path + '/test.csv')
+    test_in = test[test[0] != 'oos']
+    test_out = test[test[0] == 'oos']
+
+    dataset = {
+        "train_x":
+        data_train['text'].tolist(),
+        "train_y":
+        torch.tensor(data_train['label'].astype('category').cat.codes.tolist()),
+        "test_in":
+        test_in['text'].tolist(),
+        "test_out":
+        test_out['text'].tolist(),
+        "test_in_emb":
+        test_in['text'].tolist(),
+        "test_out_emb":
+        test_out['text'].tolist(),
+        "train_text":
+        data_train['text'].tolist()
+    }
+
+    return dataset
